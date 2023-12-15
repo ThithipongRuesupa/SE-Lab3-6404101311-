d5f8dda82544dcca8398571654f32975820c2eb3
ThithipongRuesupa <Rockqr.rocker@gmail.com>
2023-12-15 10:49:55 +0700

    add notify in framework  at quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -85,6 +85,10 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
     framework: {
+      plugins: [
+        'Notify'
+      ],
+  
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
